e7a2c34c84f1556d2e4021265f37f1f250bb774d
Sergio <sergiotmg@gmail.com>
2018-11-15 11:00:44 +0100

    2 con intents y menu

--- app/src/main/java/com/example/sergio/calculadora/MainActivity.java
+++ app/src/main/java/com/example/sergio/calculadora/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.sergio.calculadora;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     TextView txtResult;
     String result="";
+    //son codigs de peticion para que no se nos olvide de que nº es cad auno , ai no nos tenemos que acordar que el de settings era el 1
+    //el de 2 era el de help, etc,etc
+    public static int REQUEST_SETTINGS=1;
+    public static int REQUEST_HELP=2;
+    public static int REQUEST_SHARE=3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +35,65 @@ public class MainActivity extends AppCompatActivity {
         txtResult.setText(result);
         //lo tendremos que convertir a un float o l oque sea
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);// nos referimos al archivo xml creado en res/menu
+        // HAY QUE PONER EL GETMENU ARRIBA DEL RETURN!!!!!!!!!!!!!!!!!!
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        //hacerlo con un switch
+        //darme el ide de cada item que le pasemos al metodo
+        switch (item.getItemId()){
+            case  R.id.action_settings:
+                Toast.makeText(this,"Click en settings",Toast.LENGTH_LONG).show();
+                Intent intent=new Intent(this,Main2Activity.class);
+                        //si no queremos pasar datoa a la 2ª no hace falta intent.puExtra!!!
+                intent.putExtra("nombre","Nacho");//nombre es el Extramessage
+                //no hay porque cambiarle el nombre en los putExtra, recogerá nombre
+                startActivityForResult(intent,REQUEST_SETTINGS);
+            break;
+            case  R.id.action_help:
+                Toast.makeText(this,"Click en help",Toast.LENGTH_LONG).show();
+                Intent intent2=new Intent(this,Main2Activity.class);
+                intent2.putExtra("nombre","Nacho");
+                startActivityForResult(intent2,REQUEST_HELP);
+                //IMAGINAROS QUE SI EL USUARIO LOGEA SERIA UN RESULToK
+                // si fuera con un result cancel
+            break;
+            case R.id.action_share:
+                Toast.makeText(this,"Click en share",Toast.LENGTH_LONG).show();
+                Intent intent3=new Intent(this,Main2Activity.class);
+                intent3.putExtra("nombre","Nacho");
+                startActivityForResult(intent3,REQUEST_SHARE);
+            break;
+            default:
+                Toast.makeText(this,"Switch default",Toast.LENGTH_LONG).show();
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //si vienes desde tal o cual actividades haches tal o cual cosa
+        if(requestCode==REQUEST_SETTINGS){
+            if (resultCode==RESULT_OK){
+
+            }
+        }else if (requestCode==REQUEST_HELP){
+
+        }else if(requestCode==REQUEST_SHARE){
+
+        }
+    }
+
     private  int plus(int x, int y){
         return x+y;
     }
